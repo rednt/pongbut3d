e6868d511c725843fe1d606ce4b83db7512eab13
rednt <47644820+rednt@users.noreply.github.com>
2025-05-30 12:00:52 +0200

    Added ai paddle and fixed camera position

--- Paddle.cs
+++ Paddle.cs
@@ -8,10 +8,10 @@ public partial class Paddle : CharacterBody3D
     public override void _PhysicsProcess(double delta)
     {
         Vector3 dir = Vector3.Zero;
-        if (Input.IsActionPressed("ui_up")) dir.Y += 1;
-        if (Input.IsActionPressed("ui_down")) dir.Y -= 1;
-        if (Input.IsActionPressed("ui_left")) dir.Z += 1;
-        if (Input.IsActionPressed("ui_right")) dir.Z -= 1;
+        if (Input.IsActionPressed("ui_up")) dir.Z -= 1;
+        if (Input.IsActionPressed("ui_down")) dir.Z += 1;
+        if (Input.IsActionPressed("ui_left")) dir.Y += 1;
+        if (Input.IsActionPressed("ui_right")) dir.Y -= 1;
 
         Velocity = dir.Normalized() * Speed;
         MoveAndSlide();
